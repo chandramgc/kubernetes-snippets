306975b6f593ddd8033081595688661d011c42ad
girishmallula3@gmail.com <girishmallula3@gmail.com>
2020-10-19 23:08:01 -0700

    script u

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-alias k=kubectl
-alias kg="kubectl get"
-alias kd="kubectl describe"
-alias kc="kubectl create"
-alias kall="kubectl get all --all-namespaces"
+while true; do
+    alias k=kubectl
+    alias kg="kubectl get"
+    alias kd="kubectl describe"
+    alias kc="kubectl create"
+    alias kall="kubectl get all --all-namespaces"
+done
