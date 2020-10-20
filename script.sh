78a3fc93cc80d95f29110099c2d8a3328c340c9a
girishmallula3@gmail.com <girishmallula3@gmail.com>
2020-10-19 23:09:41 -0700

    u

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-while true; do
-    alias k=kubectl
-    alias kg="kubectl get"
-    alias kd="kubectl describe"
-    alias kc="kubectl create"
-    alias kall="kubectl get all --all-namespaces"
-done
+alias k=kubectl;
+alias kg="kubectl get";
+alias kd="kubectl describe";
+alias kc="kubectl create";
+alias kall="kubectl get all --all-namespaces";
